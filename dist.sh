14bbe66b420d40988b5c84ea87970d1b699f0bdc
Olivia Mackintosh <livvy@base.nu>
2021-06-23 15:33:54 +0100

    Bring branch to buildable state (update certs+scripts)
    - Certificates in nsqd/test/certs had expired so those have been
      recreated so the tests can run.
    
    - Dependency management has been moved from gpm to Go's
      mod system (gpm hasn't been maintained since 2017 so is pretty
    flaky).
    
    - GOFLAGS -> BLDFLAGS
    
    - Update Makefile so the build will try to succeed even if another
      script fails (this is kind of a kudge to work around the terrible
    build scripts of NSQ...)

--- dist.sh
+++ dist.sh
@@ -13,43 +13,41 @@
 # 11. update IRC channel topic
 # 12. tweet
 
-set -e
-
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-rm -rf   $DIR/dist/docker
-mkdir -p $DIR/dist/docker
-rm -rf   $DIR/.godeps
-mkdir -p $DIR/.godeps
-export GOPATH=$DIR/.godeps:$GOPATH
-GOPATH=$DIR/.godeps gpm install
+rm -rf   "$DIR"/dist/docker
+mkdir -p "$DIR"/dist/docker
+rm -rf   "$DIR"/.godeps
+mkdir -p "$DIR"/.godeps
+export GOPATH="$DIR"/.godeps:$GOPATH
+# GOPATH="$DIR"/.godeps gpm install
 
 GOFLAGS='-ldflags="-s -w"'
 arch=$(go env GOARCH)
-version=$(awk '/const Binary/ {print $NF}' < $DIR/internal/version/binary.go | sed 's/"//g')
+version=$(awk '/const Binary/ {print $NF}' < "$DIR"/internal/version/binary.go | sed 's/"//g')
 goversion=$(go version | awk '{print $3}')
 
 echo "... running tests"
 ./test.sh
 
-for os in linux darwin freebsd windows; do
+for os in linux darwin; do
     echo "... building v$version for $os/$arch"
     BUILD=$(mktemp -d -t nsq)
     TARGET="nsq-$version.$os-$arch.$goversion"
     GOOS=$os GOARCH=$arch CGO_ENABLED=0 \
-        make DESTDIR=$BUILD PREFIX=/$TARGET GOFLAGS="$GOFLAGS" install
-    pushd $BUILD
+        make DESTDIR="$BUILD" PREFIX=/"$TARGET" BLDFLAGS="$GOFLAGS" install
+    pushd "$BUILD" || exit
     if [ "$os" == "linux" ]; then
-        cp -r $TARGET/bin $DIR/dist/docker/
+        cp -r "$TARGET"/bin "$DIR"/dist/docker/
     fi
-    tar czvf $TARGET.tar.gz $TARGET
-    mv $TARGET.tar.gz $DIR/dist
-    popd
+    tar czvf "$TARGET".tar.gz "$TARGET"
+    mv "$TARGET".tar.gz "$DIR"/dist
+    popd || exit
     make clean
-    rm -r $BUILD
+    rm -r "$BUILD"
 done
 
-docker build -t nsqio/nsq:v$version .
+docker build -t nsqio/nsq:v"$version" .
 if [[ ! $version == *"-"* ]]; then
-    echo "Tagging nsqio/nsq:v$version as the latest release."
-    docker tag -f nsqio/nsq:v$version nsqio/nsq:latest
+    echo Tagging nsqio/nsq:v"$version" as the latest release.
+    docker tag nsqio/nsq:v"$version" nsqio/nsq:latest
 fi
---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/nsqio/nsq
+
+go 1.16
+
+require (
+	github.com/BurntSushi/toml v0.0.0-20130929175405-2dff11163ee6 // indirect
+	github.com/bitly/go-hostpool v0.0.0-20130417212715-58b95b10d6ca // indirect
+	github.com/bitly/go-simplejson v0.4.4-0.20150331164603-18db6e68d8fd // indirect
+	github.com/bitly/timer_metrics v0.0.0-20150203034119-afad1794bb13 // indirect
+	github.com/blang/semver v2.1.0+incompatible // indirect
+	github.com/bmizerany/perks v0.0.0-20140429223840-6cb9d9d72930 // indirect
+	github.com/judwhite/go-svc v1.0.0 // indirect
+	github.com/julienschmidt/httprouter v1.1.1-0.20150708215400-6aacfd5ab513 // indirect
+	github.com/mreiferson/go-options v0.0.0-20160412121024-7ae3226d3e1f // indirect
+	github.com/mreiferson/go-snappystream v0.2.3 // indirect
+	github.com/nsqio/go-nsq v1.0.6-0.20160415210702-642a3f9935f1 // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+)
